d5bc59ebd575c08d7bcb4e486ec6f4491caeabbf
Hakan Hidir <hakan.hidir@technicsatelier.com>
2022-09-26 21:57:45 +0200

    auditnet initial

---
+++ audit-api/AuditConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Audit.Core;
+using Audit.WebApi;
+using Microsoft.AspNetCore.Mvc;
+
+public static class AuditConfiguration
+{
+    public static void AddAudit(MvcOptions mvcOptions)
+    {
+        mvcOptions.AddAuditFilter(config => config
+            .LogAllActions()
+            .WithEventType("{verb} {controller}.{action}")
+            .IncludeHeaders()
+            .IncludeRequestBody()
+            .IncludeResponseHeaders()
+        );
+    }
+
+    public static void ConfigureAudit(IServiceCollection serviceCollection)
+    {
+        //To Mongo
+        Audit.Core.Configuration.Setup()
+                                 .UseMongoDB(config => config
+                                 .ConnectionString("mongodb://localhost:27017")
+                                 .Database("Audit")
+                                 .Collection("Logs"));
+
+        //To Console
+        //Audit.Core.Configuration.Setup()
+        //      .UseDynamicAsyncProvider(config => config
+        //          .OnInsert(async ev => Console.WriteLine(ev.ToJson())));
+
+        Audit.Core.Configuration.AddCustomAction(ActionType.OnEventSaving, scope =>
+        {
+            var auditAction = scope.Event.GetWebApiAuditAction();
+            if (auditAction == null)
+            {
+                return;
+            }
+
+            auditAction.Headers.Remove("Authorization");
+
+            scope.Event.CustomFields.Add("User", new { Name = "UserName", Id = "123456" });
+            scope.Event.CustomFields.Add("Application", new { Name = "AuditAPI", Node = "1" });
+
+            if (auditAction.HttpMethod.Equals("DELETE"))
+            {
+                auditAction.RequestBody = null;
+            }
+        });
+    }
+
+
+}
---
+++ audit-api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace audit_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpPost(Name = "PostWeatherForecast")]
+    public IActionResult Post([FromBody] SampleWeatherPost post)
+    {
+        return Ok();
+    }
+}
+
+public class SampleWeatherPost
+{
+    public string Name { get; set; }
+}
